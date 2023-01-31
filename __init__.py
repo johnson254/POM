9ad78c0da7c4f5f67c2a8d0be288a0d79d02b7ac
unknown <wambirujohnson@gmail.com>
2023-01-31 15:08:31 -0800

    refactored __init__.py

--- __init__.py
+++ __init__.py
@@ -1,10 +1,13 @@
-from flask import Flask, render_template, request
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask 
 #local imports
-from models import StockItem, PurchaseOrder, DeliveryNote, Invoice
+#from models import StockItem, PurchaseOrder, DeliveryNote, Invoice
+from .extensions import db
 
 def create_app():
     app = Flask(__name__)
-    db = SQLAlchemy()
+    app.config['SQLALCHEMY_DATABASE_URL'] = 'sqlite:///db.sqlite3'
+    app.config[SQLALCHEMY_TRACK_MODIFICATIONS] = False
+
+    db.init_app(app)
 
     return app
